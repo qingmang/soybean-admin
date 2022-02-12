1940d4cdc9d0f1d71b4648a968935808b185e25d
qingmang <shop@qingmang.net>
2022-02-12 13:08:41 +0800

    feat(projects): 封装组件选择

--- src/views/index.ts
+++ src/views/index.ts
@@ -2,6 +2,15 @@ import type { Component } from 'vue';
 
 type ViewComponent = Record<string, () => Promise<Component>>;
 
+type SelectComponent<T = any> =
+  | [
+      {
+        label: string;
+        value: string;
+      }
+    ]
+  | T;
+
 const importViews = import.meta.glob('./**/index.vue');
 
 const COMPONENTS_KEY = 'components';
@@ -11,6 +20,14 @@ const PATH_SPLIT_MARK = '/';
 const ROUTE_KEY_SPLIT_MARK = '_';
 /** 系统的内置路由，该文件夹名称不作为RouteKey */
 const SYSTEM_VIEW = 'system-view_';
+
+/** 排除不需要slelect的组件 */
+const excludeRouterKeys: Array<string> = [
+  COMPONENTS_KEY,
+  'system-view',
+  'exception',
+  'plugin'
+];
 
 /** 过滤掉组件文件 */
 const viewKeys = Object.keys(importViews).filter(key => !key.includes(COMPONENTS_KEY));
@@ -28,4 +45,24 @@ function getViewComponent() {
   return components;
 }
 
+/** 组合组件封装为select数据 */
+function getViewComponentSelect() {
+  //const selectKeys: Array<object> = [];
+   const selectKeys:SelectComponent = [];
+  /**去除系统乱七八糟的，只保留模块的 */
+  const filteredArr = Object.keys(importViews).filter(key => !excludeRouterKeys.some(item => key.includes(item)));
+  filteredArr.forEach(key => {
+    const routeKey = key
+      .replace(PREFIX, '')
+      .replace(SUFFIX, '')
+      .replaceAll(PATH_SPLIT_MARK, ROUTE_KEY_SPLIT_MARK)
+      .replace(SYSTEM_VIEW, '');
+
+		const path = PATH_SPLIT_MARK + key.replace(PREFIX, '').replace(SUFFIX, '');
+    selectKeys.push({ label: routeKey, value: path });
+  });
+  return selectKeys;
+}
+
 export const views = getViewComponent();
+export const viewsSelect = getViewComponentSelect();
--- src/views/multi-menu/first/second/index.vue
+++ src/views/multi-menu/first/second/index.vue
@@ -1,10 +1,27 @@
 <template>
   <div class="h-full">
-    <n-card title="多级菜单 - 二级菜单" class="h-full shadow-sm rounded-16px"></n-card>
+    <n-card title="多级菜单 - 二级菜单<组件选择>" class="h-full shadow-sm rounded-16px">
+				<n-form inline :label-width="100" ref="formRef">
+				<n-grid x-gap="12" :cols="4">
+					<n-gi>
+						<n-form-item label="请选择组件(name)" width="150" :cols="4">
+							<n-select v-model:value="value" v-model:label="label" :options="options"/>
+						</n-form-item>
+					</n-gi>
+				</n-grid>
+			</n-form>
+				<p style="color:#d20000">path : {{ value || '无' }}</p>
+		</n-card>
   </div>
 </template>
 
 <script setup lang="ts">
-import { NCard } from 'naive-ui';
+import { ref } from 'vue';
+import { NCard, NSelect, NForm, NFormItem, NGrid, NGi } from 'naive-ui';
+import { viewsSelect } from '@/views';
+
+const options = ref(viewsSelect)
+const value = ref(null)
+const label = ref(null)
 </script>
 <style scoped></style>
